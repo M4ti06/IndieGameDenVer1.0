30449a8da79b5d10e91a0b53f6ee9bff43ffec38
M4ti06 <104864383+M4ti06@users.noreply.github.com>
2023-11-14 10:49:59 +0100

    a lot of changes

--- IndieGameDen/urls.py
+++ IndieGameDen/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from blog import views
 
@@ -24,4 +26,4 @@ urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("reviews", views.ReviewsListView.as_view(), name="reviews"),
     path("reviews/article/<pk>", views.ReviewsDetailView.as_view(), name="reviews-detail")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
-from blog.models import Reviews
+from blog.models import Reviews, ReviewsImages
 
 
 class HomeView(TemplateView):
@@ -21,3 +21,12 @@ class ReviewsDetailView(DetailView):
     model = Reviews
     context_object_name = "review"
     template_name = "reviews-article.html"
+
+
+
+    def get_context_data(self, **kwargs):
+        context = super(ReviewsDetailView, self).get_context_data(**kwargs)
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        context["review_images"] = ReviewsImages.objects.filter(review=pk)
+        print(context["review_images"])
+        return context
