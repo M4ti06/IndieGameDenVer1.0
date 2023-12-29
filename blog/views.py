1bb16ede12a844d85755c869289d90cc3bd28549
M4ti06 <104864383+M4ti06@users.noreply.github.com>
2023-12-29 21:10:04 +0100

    almostdone

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
-from blog.models import Reviews, ReviewsImages, ReviewsVideo
+from blog.models import Reviews, ReviewsImages, ReviewsVideo, ReviewsComments
 
 
 class HomeView(TemplateView):
@@ -22,11 +22,10 @@ class ReviewsDetailView(DetailView):
     context_object_name = "review"
     template_name = "reviews-article.html"
 
-
-
     def get_context_data(self, **kwargs):
         context = super(ReviewsDetailView, self).get_context_data(**kwargs)
         pk = self.kwargs.get(self.pk_url_kwarg)
         context["review_images"] = ReviewsImages.objects.filter(review=pk)
         context["review_videos"] = ReviewsVideo.objects.filter(review=pk)
+        context["review_comments"] = ReviewsComments.objects.filter(game_review=pk)
         return context
